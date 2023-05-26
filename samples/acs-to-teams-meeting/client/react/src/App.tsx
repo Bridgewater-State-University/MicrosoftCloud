ca2a4db4539dc46e869e9150bf8fefddf4dd3681
bridgestateu <j12welch@bridgew.edu>
2023-05-26 12:36:57 -0400

    Final Proof-of-Concept

--- samples/acs-to-teams-meeting/client/react/src/App.tsx
+++ samples/acs-to-teams-meeting/client/react/src/App.tsx
@@ -9,6 +9,8 @@ import './App.css';
 
 const App = () => { 
   const displayName = 'Guest'
+  //const [userId, setUserId] = useState<string>('8:acs:569ebeb4-670a-4a88-a5b0-ba2037f429f6_00000018-f3a8-6c81-3dfe-9c3a0d000329');
+  //const [token, setToken] = useState<string>('eyJhbGciOiJSUzI1NiIsImtpZCI6IjVFODQ4MjE0Qzc3MDczQUU1QzJCREU1Q0NENTQ0ODlEREYyQzRDODQiLCJ4NXQiOiJYb1NDRk1kd2M2NWNLOTVjelZSSW5kOHNUSVEiLCJ0eXAiOiJKV1QifQ.eyJza3lwZWlkIjoiYWNzOjU2OWViZWI0LTY3MGEtNGE4OC1hNWIwLWJhMjAzN2Y0MjlmNl8wMDAwMDAxOC1mM2E4LTZjODEtM2RmZS05YzNhMGQwMDAzMjkiLCJzY3AiOjE3OTIsImNzaSI6IjE2ODUwMzI3MTYiLCJleHAiOjE2ODUxMTkxMTYsInJnbiI6ImFtZXIiLCJhY3NTY29wZSI6InZvaXAiLCJyZXNvdXJjZUlkIjoiNTY5ZWJlYjQtNjcwYS00YTg4LWE1YjAtYmEyMDM3ZjQyOWY2IiwicmVzb3VyY2VMb2NhdGlvbiI6InVuaXRlZHN0YXRlcyIsImlhdCI6MTY4NTAzMjcxNn0.h0UELpUe15AXOr_a3GAWZnjC51SjZh6Ey-zI_hlV2JRhoVBBY1RGWOaHvJj11vvMvawAoscG_cNRymYjQmcZQdnybl7ZLBZqRR67_zAFRLVBKBc7NSo9EYuMY8_QC5IRHW9rm-YjWJhrUJPUdIfvICGQSpIuaeinXbOKmlTn-UdDM-pWJ6ZIthfvEJrY9C3DUMPmjq-hmefnVgiIAIeecf8I9fLwoRnNW1jrhnU_rpoNBxTQqdn0zwWiimceoCIVOguMtS-fE70H72reUFjGLTIc59qtJjGPmsADBlp1_Kngn4W7w23zgCZJyE3WarXFMAKkL8WxuSwozFRuCqoOKg');
   const [userId, setUserId] = useState<string>('');
   const [token, setToken] = useState<string>('');
   const [teamsMeetingLink, setTeamsMeetingLink] = useState<string>('');
@@ -45,7 +47,9 @@ const App = () => {
 
       setMessage('Getting Teams meeting link...');
       //Call Azure Function to get the meeting link
+      console.log('Teams Function', process.env.REACT_APP_TEAMS_MEETING_FUNCTION);
       const resTeams = await fetch(process.env.REACT_APP_TEAMS_MEETING_FUNCTION as string);
+      console.log('ResTeams', resTeams);
       const link = await resTeams.text();
       setTeamsMeetingLink(link);
       setMessage('');
